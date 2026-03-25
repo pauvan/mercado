3d4fa41b7f4c5293968ba7c23fb4b51b5a9ddee2
pauvan <pauvan120@gmail.com>
2026-03-25 01:11:05 -0500

    Implementación de exception

---
+++ src/main/java/api/mercado/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package api.mercado.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
